6d0d4f6656a9364be524ae30fcbf8d9b61813fc1
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-03-26 15:05:29 -0300

    including register use case

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { type FastifyReply, type FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { registerUseCase } from '@/use-cases/register';
+
+export async function register(
+  request: FastifyRequest,
+  reply: FastifyReply
+): Promise<any> {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send();
+  }
+  return reply.status(201).send();
+}
