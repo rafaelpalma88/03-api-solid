195142aef57b90977f1085b4b0ee50caa9056845
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-04-05 11:38:52 -0300

    implementing checking distance condition

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -3,6 +3,7 @@ import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { MoreThanOneCheckinOnTheSameDayError } from './errors/more-than-one-checkin-same-day-error';
 import { GymsRepository } from '@/repositories/gyms-repository';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { calculateDistance } from '@/lib/calculateDistance';
 
 interface CheckInUseCaseRequest {
   userId: string;
@@ -23,7 +24,9 @@ export class CheckinUseCase {
 
   async execute({
     userId,
-    gymId
+    gymId,
+    userLatitude,
+    userLongitude
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
       userId,
@@ -40,7 +43,20 @@ export class CheckinUseCase {
       throw new ResourceNotFoundError();
     }
 
-    // implementar aqui questao da geolocalizacao
+    if (!userLatitude || !userLongitude) {
+      throw new ResourceNotFoundError(); // erro pois nao temos a latitude e longitude do usuário
+    }
+
+    const distanceBetweenUserAndGym = calculateDistance({
+      lat1: Number(gym.latitude),
+      lon1: Number(gym.longitude),
+      lat2: Number(userLatitude),
+      lon2: Number(userLongitude)
+    });
+
+    if (distanceBetweenUserAndGym > 150) {
+      throw new ResourceNotFoundError(); // erro pois a distancia é maior que 150 metros
+    }
 
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
