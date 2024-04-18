66048c6fa541e37452630ff4d4bffb7c5611e8dd
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2024-04-18 14:04:51 -0300

    implementing jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { type FastifyInstance } from 'fastify';
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
 import { profile } from './controllers/profile';
+import { verifyJwt } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance): Promise<any> {
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
   /* Authenticated */
-  app.get('/me', profile);
+  app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
